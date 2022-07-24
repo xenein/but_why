5a0cbd3616e6bce3ce408f225c8d562dba824c69
xenia <xhartmann@posteo.de>
2022-07-24 21:28:10 +0200

    mock, mock, who's there?

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 import json
 
-from flask import Flask
+from flask import Flask, render_template
 
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
 
     with open("./db_config.json") as fp:
         config = json.load(fp)
 
     app.config.update(config)
+
+    @app.route("/")
+    def start_view() -> str:
+        return render_template("start.html")
+
+    @app.route("/questions/")
+    def questions_view() -> str:
+        return render_template("questions.html")
 
     return app
